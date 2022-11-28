8b64ffd7fc1fa4b530f7a1bd4b7f0901c0648e84
maulik4889 <maulik4889@gmail.com>
2022-11-28 10:16:10 +0000

    COmmit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <div style="text-align:center; font-size:100px">Matutto</div>
+            <div style="text-align:center; font-size:100px; Color:red">Matutto</div>
         </div>
     </body>
 </html>
